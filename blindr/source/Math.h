9b0a82f9994d6641a5fe5be982bcb17594371269
Max Kaufmann <max.kaufmann@gmail.com>
2013-10-01 13:26:55 -0700

    syntax changes for windows

--- blindr/source/Math.h
+++ blindr/source/Math.h
@@ -27,6 +27,9 @@
 inline float clamp(float u, float lo=0.f, float hi=1.f) { return u<lo?lo:u>hi?hi:u; }
 inline float lerp(float u, float v, float t) { return u + t * (v - u); }
 
+struct vec2;
+inline vec2 vec(float ax, float ay);
+
 struct vec2 {
 	float x;
 	float y;
@@ -36,21 +39,21 @@ struct vec2 {
 	float norm() const         { return x*x + y*y; }
 	float manhattan() const    { return fabs(x)+fabs(y); }
 	float magnitude() const    { return sqrtf(norm()); }
-	vec2 conjugate() const     { return {x,-y}; }
+	vec2 conjugate() const     { return vec(x,-y); }
 	float radians() const      { return atan2f(y,x); }
-	vec2 reflection() const    { return {y,x}; }
-	vec2 anticlockwise() const { return {-y, x}; }
-	vec2 clockwise() const     { return {y, -x}; }
+	vec2 reflection() const    { return vec(y,x); }
+	vec2 anticlockwise() const { return vec(-y, x); }
+	vec2 clockwise() const     { return vec(y, -x); }
 	vec2 normalized() const    { return (*this) / magnitude(); }
 
-	vec2 operator+(vec2 q) const  { return {x+q.x, y+q.y}; }
-	vec2 operator-(vec2 q) const  { return {x-q.x, y-q.y}; }
-	vec2 operator*(vec2 q) const  { return {x*q.x, y*q.y}; }
-	vec2 operator/(vec2 q) const  { return {x/q.x, y/q.y}; }
+	vec2 operator+(vec2 q) const  { return vec(x+q.x, y+q.y); }
+	vec2 operator-(vec2 q) const  { return vec(x-q.x, y-q.y); }
+	vec2 operator*(vec2 q) const  { return vec(x*q.x, y*q.y); }
+	vec2 operator/(vec2 q) const  { return vec(x/q.x, y/q.y); }
 
-	vec2 operator-() const        { return {-x, -y}; }
-	vec2 operator*(float k) const { return {k*x, k*y}; }
-	vec2 operator/(float k) const { return {x/k, y/k}; }
+	vec2 operator-() const        { return vec(-x, -y); }
+	vec2 operator*(float k) const { return vec(k*x, k*y); }
+	vec2 operator/(float k) const { return vec(x/k, y/k); }
 
 	vec2 operator +=(vec2 u)  { x+=u.x; y+=u.y; return *this; }
 	vec2 operator -=(vec2 u)  { x-=u.x; y-=u.y; return *this; }
@@ -61,13 +64,18 @@ struct vec2 {
 	vec2 operator /=(float k) { x/=k; y/=k; return *this; }
 };
 	
-inline vec2 vec(float ax, float ay) { return {ax, ay}; }
-inline vec2 cmul(vec2 u, vec2 v) { return {u.x*v.x-u.y*v.y, u.x*v.y+u.y*v.x}; }
+vec2 vec(float ax, float ay) {
+	vec2 result;
+	result.x = ax;
+	result.y = ay;
+	return result;
+}
+inline vec2 cmul(vec2 u, vec2 v) { return vec(u.x*v.x-u.y*v.y, u.x*v.y+u.y*v.x); }
 inline vec2 cdiv(vec2 u, vec2 v) {
 	float normInv = 1.0f/v.norm();
-	return {(u.x*v.x+u.y*v.y)*normInv, (v.x*u.y-u.x*v.y)*normInv};
+	return vec((u.x*v.x+u.y*v.y)*normInv, (v.x*u.y-u.x*v.y)*normInv);
 }
-inline vec2 polar(float radius, float radians) { return {radius*cosf(radians), radius*sinf(radians)}; }
+inline vec2 polar(float radius, float radians) { return vec(radius*cosf(radians), radius*sinf(radians)); }
 inline float easeOut2(float u) {
 	u=1.0-u;
 	return 1.0 - u*u;
@@ -83,7 +91,7 @@ inline double randomValue() { return rand() / double(RAND_MAX); }
 inline double randomValue(double u, double v) { return u + randomValue() * (v - u); }
 inline double expovariate(double avgDuration) { return -avgDuration * log(1.0 - randomValue(0.0000001, 0.999999)); }
 	
-inline vec2 operator*(float k, vec2 q) { return {k*q.x, k*q.y}; }
+inline vec2 operator*(float k, vec2 q) { return vec(k*q.x, k*q.y); }
 inline float dot(vec2 u, vec2 v) { return u.x*v.x + u.y*v.y; }
 inline float cross(vec2 u, vec2 v) { return u.x * v.y - v.x* u.y; }
 inline vec2 lerp(vec2 u, vec2 v, float t) { return u + t * (v - u); }
@@ -124,6 +132,9 @@ vec2 cubicBezierDeriv(vec2 p0, vec2 p1, vec2 p2, vec2 p3, float u);
 vec2 cubicHermite(vec2 p0, vec2 m0, vec2 p1, vec2 m1, float u);
 vec2 cubicHermiteDeriv(vec2 p0, vec2 m0, vec2 p1, vec2 m1, float u);
 
+struct AABB;
+inline AABB aabb(float x, float y, float w, float h);
+
 struct AABB {
 	float x, y, w, h;
 	
@@ -132,24 +143,35 @@ struct AABB {
 	vec2 extentMax() const { return extentMin() + size(); }
 	
 	bool contains(vec2 p) { return p.x > x && p.y > y && p.x < x+w && p.y < y+h; }
-	AABB inflate(float pad) { return { x-pad, y-pad, w+pad+pad, h+pad+pad }; }
+	AABB inflate(float pad) { return aabb( x-pad, y-pad, w+pad+pad, h+pad+pad ); }
 };
 
-inline AABB aabb(float x, float y, float w, float h) { return { x, y, w, h }; }
+inline AABB aabb(float x, float y, float w, float h) {
+	AABB result = { x, y, w, h };
+	return result;
+}
 inline AABB aabb(vec2 p, vec2 s) { return { p.x, p.y, s.x, s.y }; }
 inline AABB unionOf(AABB u, AABB v) { return aabb(MIN(u.x, v.x), MIN(u.y, v.y), MAX(u.x+u.w, v.x+v.w), MAX(u.y+u.h, v.y+v.h)); }
+
+struct ivec2;
+inline ivec2 ivec(int x, int y);
 
 struct ivec2 {
 	int x, y;
 	
 	inline vec2 toFloat() { return vec(x, y); }
-	inline ivec2 operator+(const ivec2& rhs) { return {x+rhs.x, y+rhs.y}; }
-	inline ivec2 operator-(const ivec2& rhs) { return {x-rhs.x, y-rhs.y}; }
-	inline ivec2 operator*(const ivec2& rhs) { return {x*rhs.x, y*rhs.y}; }
-	inline ivec2 operator/(const ivec2& rhs) { return {x/rhs.x, y/rhs.y}; }
+	inline ivec2 operator+(const ivec2& rhs) { return ivec(x+rhs.x, y+rhs.y); }
+	inline ivec2 operator-(const ivec2& rhs) { return ivec(x-rhs.x, y-rhs.y); }
+	inline ivec2 operator*(const ivec2& rhs) { return ivec(x*rhs.x, y*rhs.y); }
+	inline ivec2 operator/(const ivec2& rhs) { return ivec(x/rhs.x, y/rhs.y); }
 };
 
-inline ivec2 ivec(int x, int y) { return {x, y}; }
+inline ivec2 ivec(int x, int y) {
+	ivec2 result;
+	result.x = x;
+	result.y = y;
+	return result;
+}
 inline ivec2 floor(vec2 v) { return { int(v.x), int(v.y) }; }
 
 
